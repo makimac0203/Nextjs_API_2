d491c79ef5fd94cf05af118ec827ee7ff3de24dc
hirotaka_maki <h.maki@laniakea-tech.com>
2025-08-19 14:02:11 +0900

    fix

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -141,5 +141,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic でここに集約される
+STATIC_URL = '/static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
