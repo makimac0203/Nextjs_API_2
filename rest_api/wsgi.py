aee92deed2b4de5bac1deba02082cfca3e1d850f
hirotaka_maki <h.maki@laniakea-tech.com>
2025-08-19 12:22:36 +0900

    fix

--- rest_api/wsgi.py
+++ rest_api/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for rest_api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
+from whitenoise import WhiteNoise 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rest_api.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'static'))
